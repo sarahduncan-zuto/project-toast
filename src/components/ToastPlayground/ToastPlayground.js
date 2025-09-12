75dbe7da5609f3635b1ca18758fe2a8a5d34f880
Sarah Duncan <sarah.duncan@zuto.com>
2025-09-12 21:39:19 +0100

    adds exercises 2 and 3

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Button from "../Button";
 import styles from "./ToastPlayground.module.css";
+import ToastShelf from "../ToastShelf";
 
 const VARIANT_OPTIONS = ["notice", "warning", "success", "error"];
 
 function ToastPlayground() {
   const [message, setMessage] = React.useState("");
   const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0]);
+  const [messages, setMessages] = React.useState([]);
 
   const handleVariantChange = (e) => {
     setVariant(e.target.value);
@@ -16,14 +18,42 @@ function ToastPlayground() {
     setMessage(e.target.value);
   };
 
+  const handleSubmitMessage = (e) => {
+    e.preventDefault();
+    const existingMessages = [...messages];
+    const newMessage = {
+      id: crypto.randomUUID(),
+      message: message,
+      variant: variant,
+    };
+    existingMessages.push(newMessage);
+    setMessages(existingMessages);
+    setMessage("");
+    setVariant(VARIANT_OPTIONS[0]);
+  };
+
+  const closeToast = (id) => {
+    const existingMessages = [...messages];
+    const filteredMessages = existingMessages.filter(
+      (message) => message.id !== id
+    );
+    setMessages(filteredMessages);
+  };
+
+  const messagesWithCloseModal = messages.map((message) => {
+    return { ...message, closeModal: () => closeToast(message.id) };
+  });
+
   return (
     <div className={styles.wrapper}>
       <header>
         <img alt="Cute toast mascot" src="/toast.png" />
         <h1>Toast Playground</h1>
       </header>
-
-      <div className={styles.controlsWrapper}>
+      <form
+        onSubmit={(e) => handleSubmitMessage(e)}
+        className={styles.controlsWrapper}
+      >
         <div className={styles.row}>
           <label
             htmlFor="message"
@@ -65,10 +95,11 @@ function ToastPlayground() {
         <div className={styles.row}>
           <div className={styles.label} />
           <div className={`${styles.inputWrapper} ${styles.radioWrapper}`}>
-            <Button>Pop Toast!</Button>
+            <Button type="submit">Pop Toast!</Button>
           </div>
         </div>
-      </div>
+      </form>
+      <ToastShelf messages={messagesWithCloseModal} />
     </div>
   );
 }
--- src/components/ToastShelf/ToastShelf.js
+++ src/components/ToastShelf/ToastShelf.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React from "react";
 
-import Toast from '../Toast';
-import styles from './ToastShelf.module.css';
+import Toast from "../Toast";
+import styles from "./ToastShelf.module.css";
 
-function ToastShelf() {
+function ToastShelf({ messages = [] }) {
   return (
     <ol className={styles.wrapper}>
-      <li className={styles.toastWrapper}>
-        <Toast variant="notice">Example notice toast</Toast>
-      </li>
-      <li className={styles.toastWrapper}>
-        <Toast variant="error">Example error toast</Toast>
-      </li>
+      {messages.map((message) => (
+        <li className={styles.toastWrapper} key={message.id}>
+          <Toast variant={message.variant} closeModal={message.closeModal}>
+            {message.message}
+          </Toast>
+        </li>
+      ))}
     </ol>
   );
 }
